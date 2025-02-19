fe117bbb44891e0aa41b10b294e7c8b9a9b00b1e
Felipe <felipetem2004@gmail.com>
2025-02-19 13:49:01 -0300

    funcionalidade principal adicionada

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 export const overlayfadeOut = function () {
-  const overlay = document.querySelector(".form__overlay"); // Seletor do seu overlay
+  const overlay = document.querySelector(".form__overlay"); // 
   let opacity = 1;
   const fadeOutInterval = setInterval(() => {
     opacity -= 0.05;
@@ -12,32 +12,72 @@ export const overlayfadeOut = function () {
   }, 50);
 };
 
-export const formActiveBtn = function () {
+export const initEvents = function () {
+  updateCarousel();
+  addToggleDropdown();
+  formActiveBtn();
+  updateCarousel();
+  formHelper();
+  getImg();
+};
+
+const addToggleDropdown = function () {
+  document.body.addEventListener("click", function (event) {
+    if (event.target && event.target.classList.contains("w-dropdown-toggle")) {
+      const dropdown = event.target.closest(".w-dropdown");
+      const dropdownList = dropdown.querySelector(".w-dropdown-list");
+
+      if (dropdown && dropdownList) {
+        const isOpen = dropdown.classList.contains("w--open");
+
+        dropdown.classList.toggle("w--open");
+        dropdownList.classList.toggle("w--open");
+
+        event.target.setAttribute("aria-expanded", isOpen ? "false" : "true");
+
+        if (isOpen) {
+          dropdown.style.zIndex = "";
+        } else {
+          dropdown.style.zIndex = "901";
+        }
+
+        console.log("Estado do dropdown:", isOpen ? "Fechado" : "Aberto");
+      }
+    }
+  });
+};
+
+const formHelper = function () {
+  const form = document.querySelector(".form__content");
+  form.addEventListener("click", function (e) {
+    e.stopPropagation();
+  });
+};
+
+const formActiveBtn = function () {
   const priorityBtnsContainer = document.querySelector(".priority__btns");
+
   priorityBtnsContainer.addEventListener("click", function (e) {
     e.preventDefault();
     const btn = e.target;
+
+    console.log(e.target);
     console.log("a");
+
     const priorityBtns = priorityBtnsContainer.querySelectorAll(
       ".btn_low, .btn_medium, .btn__high"
     );
-    priorityBtns.forEach((otherBtn) => {
-      otherBtn.classList.remove(
-        "btn_low--active",
-        "btn_medium--active",
-        "btn__high--active"
-      );
-    });
+    if (!btn.classList.contains(`${btn.classList[0]}--active`)) {
+      priorityBtns.forEach((otherBtn) => {
+        otherBtn.classList.remove(
+          "btn_low--active",
+          "btn_medium--active",
+          "btn__high--active"
+        );
+      });
+    }
 
-    if (btn.classList.contains("btn_low")) {
-      btn.classList.toggle("btn_low--active");
-    }
-    if (btn.classList.contains("btn_medium")) {
-      btn.classList.toggle("btn_medium--active");
-    }
-    if (btn.classList.contains("btn__high")) {
-      btn.classList.toggle("btn__high--active");
-    }
+    btn.classList.toggle(`${btn.classList[0]}--active`);
   });
 };
 export const cleanBtns = function () {
@@ -64,7 +104,7 @@ export const getPriority = function () {
     let prioridade = "";
 
     if (activeBtn.classList.contains("btn_low")) {
-      prioridade = "Low";
+      prioridade = "low";
     } else if (activeBtn.classList.contains("btn_medium")) {
       prioridade = "Medium";
     } else if (activeBtn.classList.contains("btn__high")) {
@@ -77,7 +117,6 @@ export const getPriority = function () {
 export const getDate = function () {
   let today = new Date();
 
-  // Meses do ano em formato abreviado
   const months = [
     "Jan",
     "Feb",
@@ -102,3 +141,118 @@ export const getDate = function () {
 
   return formattedDate;
 };
+
+export let selectedImg = ""; // Variável global para armazenar a imagem selecionada
+
+export const getImg = function () {
+  const carousel = document.querySelector(".carousel");
+
+  carousel.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const fullImgSrc = e.target.src;
+    selectedImg = fullImgSrc.split("/").slice(3).join("/"); // Armazenando o valor da imagem
+
+    console.log(selectedImg); // Exibindo o caminho relativo no console
+  });
+};
+
+const updateCarousel = function () {
+  const track = document.querySelector(".carousel");
+  const prevButton = document.querySelector(".prev");
+  const nextButton = document.querySelector(".next");
+  const items = document.querySelectorAll(".carousel-item");
+  const totalItems = items.length / 3;
+  let currentIndex = 0;
+
+  function updateTransform() {
+    track.style.transform = `translateX(-${100 * currentIndex}%)`;
+  }
+
+  prevButton.addEventListener("click", function () {
+    currentIndex--;
+    console.log(currentIndex);
+    if (currentIndex < 0) {
+      currentIndex = totalItems - 1;
+      updateTransform();
+    } else {
+      updateTransform();
+    }
+  });
+
+  nextButton.addEventListener("click", function () {
+    currentIndex++;
+    console.log(currentIndex);
+    if (currentIndex >= totalItems) {
+      currentIndex = 0;
+      updateTransform();
+    } else {
+      updateTransform();
+    }
+  });
+};
+
+export const createTaskHTML = (title, description, priority, date, img) => {
+  const formattedPriority =
+    priority && typeof priority === "string"
+      ? priority.toLowerCase()
+      : "default";
+
+  return `
+    <div class="w-layout-blockcontainer main__container--2 w-container">
+      <div class="w-layout-blockcontainer main__content--2 w-container">
+        <div class="w-layout-blockcontainer container-8 w-container"></div>
+        <div class="w-layout-blockcontainer header__in--review w-container">
+          <div class="div-block-2">
+            <div class="priority__progress priority__${formattedPriority}">
+              ${priority}
+            </div>
+            <div class="date__progress">
+              <span class="text-span-2"> </span>${date}
+            </div>
+          </div>
+          <div class="div-block-3">
+            <div class="button__opt--in--progress w-dropdown">
+              <div class="dropdown-toggle w-dropdown-toggle" aria-expanded="false" role="button">
+                <div class="text-block"></div>
+              </div>
+              <nav class="task__progress--opt w-dropdown-list">
+                <a href="#" class="task__progress--opt-1 w-dropdown-link">Delete</a>
+                <a href="#" class="task__progress--opt-1 w-dropdown-link">Edit</a>
+                <a href="#" class="task__progress--opt-1 w-dropdown-link" id="move-to-finished">Mover para Concluído</a>
+              </nav>
+            </div>
+          </div>
+        </div>
+        <div class="w-layout-blockcontainer in__progress--img---container w-container">
+          <img src="${img}" loading="lazy" alt="" class="in__progress-img" />
+        </div>
+        <div class="w-layout-blockcontainer in__progress--title w-container">
+          <h3 class="task__progress-title">${title}</h3>
+        </div>
+        <div class="w-layout-blockcontainer in__progress-description w-container">
+          <div class="task__progress-description">${description}</div>
+        </div>
+      </div>
+    </div>
+  `;
+};
+export function insertTaskHTML(section, taskHTML) {
+  let sectionElement;
+
+  switch (section) {
+    case "todo__section":
+      sectionElement = document.querySelector(".todo__section");
+      break;
+    case "in__progress-section":
+      sectionElement = document.querySelector(".in__progress-section");
+      break;
+    case "in__review--section":
+      sectionElement = document.querySelector(".in__review--section");
+      break;
+    default:
+      return;
+  }
+
+  sectionElement.insertAdjacentHTML("beforeend", taskHTML);
+}
--- script.js
+++ script.js
@@ -1,73 +1,60 @@
-import { overlayfadeOut } from "./helpers.js";
-import { formActiveBtn } from "./helpers.js";
-import { cleanBtns } from "./helpers.js";
-import { getPriority } from "./helpers.js";
-import { getDate } from "./helpers.js";
+import {
+  overlayfadeOut,
+  cleanBtns,
+  getPriority,
+  getDate,
+  createTaskHTML,
+  insertTaskHTML,
+  selectedImg,
+  initEvents,
+} from "./helpers.js";
 
-const todoSection = document.querySelector(".todo__section"); // Seletor da seção todo
-const todoMainContainer = document.querySelector(".main__container--1"); // container do conteúdo principal todo
-const form = document.querySelector(".form__content"); // Seletor do seu formulário
-const submitButton = document.querySelector(".btn__form--submit"); // Seletor do botão de submit
-const btnAddTaskTodo = document.querySelector(".btn__add--new--task"); // Seletor do botão de adicionar tarefa
+const inProgressSection = document.querySelector(".in__progress-section");
+const todoSection = document.querySelector(".todo__section");
+const form = document.querySelector(".form__content");
+const submitButton = document.querySelector(".btn__form--submit");
+const addBtns = document.querySelectorAll(".btn__add--task--2");
 
-const newMainContainer = `<div class="w-layout-blockcontainer main__container--2 w-container">
-<div class="w-layout-blockcontainer main__content--2 w-container">
-<div class="w-layout-blockcontainer container-8 w-container">
-</div>
-<div class="w-layout-blockcontainer header__in--review w-container">
-<div class="div-block-2">
-<div class="priority__progress">Medium</div>
-<div class="date__progress">
-<span class="text-span-2"> </span>nov 10, 2004
-</div>
-</div>
-<div class="div-block-3">
-<div data-hover="false" data-delay="0" class="button__opt--in--progress w-dropdown">
-<div class="dropdown-toggle w-dropdown-toggle" id="w-dropdown-toggle-12" aria-controls="w-dropdown-list-12" aria-haspopup="menu" aria-expanded="false" role="button" tabindex="0">
-<div class="text-block"></div>
-</div>
-<nav class="task__progress--opt w-dropdown-list" id="w-dropdown-list-12" aria-labelledby="w-dropdown-toggle-12">
-<a href="#" class="task__progress--opt-1 w-dropdown-link" tabindex="0">Delete</a><a data-w-id="4fe991ce-97f3-d3aa-2ed1-06ad70cc964f" href="#" class="task__progress--opt-1 w-dropdown-link" tabindex="0">Edit</a>
-</nav>
-</div>
-</div>
-</div>
-<div class="w-layout-blockcontainer in__progress--img---container w-container">
-<img src="https://cdn.prod.website-files.com/6796f237a18f3d49c7ea494f/6797146abd2d61df075c3b40_The%20Ultimate%20Starter%20Bundle.png" loading="lazy" alt="" class="in__progress-img">
-</div>
-<div class="w-layout-blockcontainer in__progress--title w-container">
-<h3 class="task__progress-title">Task title</h3>
-</div>
-<div class="w-layout-blockcontainer in__progress-description w-container">
-<div class="task__progress-description">
-Tasks that you have added will appear here 😍
-</div>
-</div>
-</div>
-</div>`;
+const checkIt = {
+  currentSelectedSection: "",
 
-const formHelper = function () {
-  form.addEventListener("click", function (e) {
-    e.stopPropagation();
-    formActiveBtn();
-  });
+  init() {
+    document.addEventListener("DOMContentLoaded", initEvents);
+
+    addBtns.forEach((el) => {
+      el.addEventListener("click", function () {
+        checkIt.currentSelectedSection = el.getAttribute("data-section");
+        console.log(checkIt.currentSelectedSection);
+      });
+    });
+
+    submitButton.addEventListener("click", () => {
+      if (!checkIt.currentSelectedSection) return; // Não fazer nada se nenhuma seção foi selecionada
+
+      overlayfadeOut();
+
+      const title = document.querySelector(".form__task--title").value;
+      const taskDescription = document.querySelector(
+        ".form__task--description"
+      ).value;
+      const priority = getPriority();
+      const date = getDate();
+      const img = selectedImg;
+      console.log(img);
+
+      const taskHTML = createTaskHTML(
+        title,
+        taskDescription,
+        priority,
+        date,
+        img
+      );
+      insertTaskHTML(checkIt.currentSelectedSection, taskHTML);
+
+      form.reset();
+      cleanBtns();
+    });
+  },
 };
-formHelper();
 
-submitButton.addEventListener("click", (e) => {
-  overlayfadeOut();
-
-  const title = document.querySelector(".form__task--title").value;
-  const taskDescription = document.querySelector(
-    ".form__task--description"
-  ).value;
-  const prioridade = getPriority();
-  const date = getDate();
-  console.log(prioridade);
-  console.log(title);
-  console.log(taskDescription);
-  console.log(date);
-
-  form.reset();
-  cleanBtns();
-});
+checkIt.init();
